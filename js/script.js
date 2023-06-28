94e94f8b9d5cb051e911f06269495a1d6962f4ac
Stephen Suresh <stephen.suresh@outlook.com>
2023-06-27 20:10:38 -0400

    styling and grid creation

--- js/script.js
+++ js/script.js
@@ -1,13 +1,41 @@
 const board = document.querySelector("#drawing-board");
-let rows = 5;
-let columns = 5;
+const slider = document.querySelector('#grid-size');
+let color = 'black';
+createGrid(slider.value);
 
-let grid;
-for (let i = 1; i <= rows; i++) {
-    grid = document.createElement('div');
-    for (let j = 1; j <= columns; i++) {
-        let section = document.createElement('div');
-        grid.appendChild(section);
+slider.addEventListener('change', (e) => {
+    createGrid(slider.value);
+})
+
+function createGrid(rowsAndCols) {
+    board.innerHTML = "";
+    let grid;
+    for (let i = 0; i < rowsAndCols; i++) {
+        grid = document.createElement('div');
+        grid.classList.add('row');
+        for (let j = 0; j < rowsAndCols; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            // cell.setAttribute('draggable','false');
+            cell.addEventListener('mouseover', (e) => {
+                colorIn(e, color);
+            })
+            grid.appendChild(cell);
+        }
+        board.appendChild(grid);
     }
-    board.appendChild(div);
+}
+
+
+
+function colorIn(e, selectedColor) {
+    //if buttons are not pressed
+    // console.log(e.buttons)
+    if (e.buttons !== 1) return;
+    if (selectedColor === 'black') {
+        e.target.classList.add('filled-black')
+    }
+    else if (selectedColor === 'rainbow') {
+
+    }
 }
